4aa0db7afc6b4f1894756cb8e8814c679114e88a
Ranger1986 <pierre6richard7@gmail.com>
2023-11-24 11:25:16 +0100

    la sound

--- Wave.hpp
+++ Wave.hpp
@@ -7,54 +7,54 @@ class Wave {
 private :
   //chunk type
   char file_type[4];         // (4 octets) : Constante "RIFF" i.e identification du format
-  int file_size;             // (4 octets) : file_size est le nombre d'octet restant e lire (i.e = taille du fichier moins 8 octets)
+  int file_size;             // (4 octets) : file_size est le nombre d'octet restant à lire (i.e = taille du fichier moins 8 octets)
   char file_id[4];           // (4 octets) : Identifiant "WAVE"
   //chunk format
   char chunk_id[4];          // (4 octets) : Identifiant "fmt "
-  int chunk_size;            // (4 octets) : Nombre d'octets utilises pour definir en detail le chunk
+  int chunk_size;            // (4 octets) : Nombre d'octets utilisés pour définir en détail le chunk
   short format;              // (2 octets) : Format de fichier (1: PCM,  ...)
-  short channels_nb;         // (2 octets) : Nombre de canaux (1 pour mono ou 2 pour stereo)
-  int sampling_freq;         // (4 octets) : Frequence d'echantillonnage (en Hertz)
+  short channels_nb;         // (2 octets) : Nombre de canaux (1 pour mono ou 2 pour stéréo)
+  int sampling_freq;         // (4 octets) : Fréquence d'échantillonnage (en Hertz)
   int bytes_per_second;      // (4 octets) : Nombre d'octets par seconde de musique
-  short bytes_per_sample;    // (2 octets) : Nombre d'octets par echantillon
-  short depth;               // (2 octets) : Nombre de bits par donnee (8 ou 16)
-  //chunk donnees
+  short bytes_per_sample;    // (2 octets) : Nombre d'octets par échantillon
+  short depth;               // (2 octets) : Nombre de bits par donnée (8 ou 16)
+  //chunk données
   char data_id[4];          // (4 octets) : Constante "data"
   int data_size;            // (4 octets) : nombre d'octet restant (i.e = taille du fichier moins 44 octets)
   //data
-  unsigned char* data8;     // Tableau de donees lorsque l'on est sur des donnees 8 bits
-  short* data16;            // Tableau de donees lorsque l'on est sur des donnees 16 bits
-  long int data_nb;         // Nombre de donnees
+  unsigned char* data8;     // Tableau de donées lorsque l'on est sur des données 8 bits
+  short* data16;            // Tableau de donées lorsque l'on est sur des données 16 bits
+  long int data_nb;         // Nombre de données
 
-  bool is_data8_allocated;  // Indique si le tableau e ete alloue
-  bool is_data16_allocated; // Indique si le tableau e ete alloue
+  bool is_data8_allocated;  // Indique si le tableau à été alloué
+  bool is_data16_allocated; // Indique si le tableau à été alloué
   
   static void checkTypesSize(); // Test la taille des types
 
-  void InitDescriptor(int depth,          // Nombre de bits par donnee (8 ou 16)
-                      short channels_nb,  // Nombre de canaux (1 pour mono ou 2 pour stereo)
-                      int sampling_freq); // Frequence d'echantillonnage (en Hertz)(classique en musique : 44100 Hz)
+  void InitDescriptor(int depth,          // Nombre de bits par donnée (8 ou 16)
+                      short channels_nb,  // Nombre de canaux (1 pour mono ou 2 pour stéréo)
+                      int sampling_freq); // Fréquence d'échantillonnage (en Hertz)(classique en musique : 44100 Hz)
 
 public:
        
-  //Methode permettant de recuperer les donnees (cad le signal sur 8 bits)     
-  void getData8(unsigned char** data, // pointeur sur le tableau de donees lorsque l'on est sur des donnees 8 bits
+  //Méthode permettant de récupérer les données (cad le signal sur 8 bits)     
+  void getData8(unsigned char** data, // pointeur sur le tableau de donées lorsque l'on est sur des données 8 bits
                 int* size);           // pointeur sur la taille du tableau 
-  //Methode permettant de modifier les donnees
-  void modifData8(unsigned char* data); // Tableau de donees lorsque l'on est sur des donnees 8 bits
+  //Méthode permettant de modifier les données
+  void modifData8(unsigned char* data); // Tableau de donées lorsque l'on est sur des données 8 bits
 
   void read(char* fileName);   //Lecture d'un fichier wave
   void write(char* fileName);
        
   Wave();
   ~Wave();
-  Wave(short* data16,      // Tableau de donnees
-       long int data_nb,   // Nombre de donnees
-       short channels_nb,  // Nombre de canaux (1 pour mono ou 2 pour stereo)
-       int sampling_freq); // Frequence d'echantillonnage (en Hertz)(classique en musique : 44100 Hz)
-  Wave(unsigned char* data8, // Tableau de donees lorsque l'on est sur des donnees 8 bits
-       long int data_nb,     // Nombre de donnees
-       short channels_nb,    // Nombre de canaux (1 pour mono ou 2 pour stereo)
-       int sampling_freq);   // Frequence d'echantillonnage (en Hertz)(classique en musique : 44100 Hz)
+  Wave(short* data16,      // Tableau de données
+       long int data_nb,   // Nombre de données
+       short channels_nb,  // Nombre de canaux (1 pour mono ou 2 pour stéréo)
+       int sampling_freq); // Fréquence d'échantillonnage (en Hertz)(classique en musique : 44100 Hz)
+  Wave(unsigned char* data8, // Tableau de donées lorsque l'on est sur des données 8 bits
+       long int data_nb,     // Nombre de données
+       short channels_nb,    // Nombre de canaux (1 pour mono ou 2 pour stéréo)
+       int sampling_freq);   // Fréquence d'échantillonnage (en Hertz)(classique en musique : 44100 Hz)
 };
 #endif //IO_WAVE_HPP
--- main.cpp
+++ main.cpp
@@ -1,11 +1,116 @@
-#include <iostream>
-#include <stdlib.h>
-#include <fstream>
-#include <math.h>
 #include "Wave.hpp"
+#include "Wave.cpp"
+#include <iostream>
+#include <cmath>
 
-int main()
+void creation_note(double Freq, double fe, int nbechantillons, double *realdata)
 {
-    Wave son=Wave();
-    return 0;
+	for (size_t i = 0; i < nbechantillons; i++)
+	{
+		realdata[i] = sin(((double)i / fe) * Freq * 2 * M_PI) * 0.5;
+	}
 }
+
+unsigned char doubleToUnsignedChar(double nombre)
+{
+	if (nombre > 1)
+		return 255;
+	if (nombre < -1)
+		return 0;
+	return floor((nombre + 1) / 2 * 255);
+}
+
+double unsignedCharToDouble(unsigned char caractere)
+{
+	return (double)caractere / 127.5 - 1;
+}
+
+int main(int argc, char **argv)
+{
+	unsigned char *data8;
+	int data_nb;
+	short mono = 1;
+
+	double laFreq = 440.0;
+	double fe = 44100.0;
+	int nbechantillons;
+	double temps = 6.0;
+	double *realdata;
+
+	nbechantillons = (int)floor(temps * fe);
+	int m = ceil(log2(nbechantillons));
+	//nbechantillons = pow(2, m);
+
+	realdata = (double *)malloc(pow(2, m) * sizeof(double));
+	for (size_t i = 0; i < pow(2, m); i++)
+	{
+		realdata[i] = 0;
+	}
+	int frequence = 440;
+	for (char i = 0; i < 1; i++)
+	{
+		creation_note(frequence, fe, nbechantillons, realdata);
+
+	}
+	data8 = new unsigned char[nbechantillons];
+
+	for (int i = 0; i < nbechantillons; i++)
+	{
+		data8[i] = doubleToUnsignedChar(realdata[i]);
+	}
+
+	Wave la2(data8, nbechantillons, mono, fe);
+
+	// la.modifData8(data8);
+	la2.write("test.wav");
+	return 0;
+}
+
+/*
+#include "Wave.cpp"
+#include "Wave.hpp"
+#include <iostream>
+#include <cstdlib>
+
+int CreerNote(double freq_note, double freq_ech, int nb_ech, double *signal){
+	double alpha = 2.0 * M_PI*freq_note/freq_ech;
+	for (int i = 0; i < nb_ech; i++)
+	{
+		signal[i] = sin(alpha*(double)i);
+	}
+	
+	return 1;
+}
+unsigned char dtoc(double nombre)
+{
+	if (nombre > 1)
+		return 255;
+	if (nombre < -1)
+		return 0;
+	return floor((nombre + 1) / 2 * 255);
+}
+
+int main(){
+	double frequence_note = 440;
+	int sampling_freq = 100;
+	long int data_nb = 600;
+	short channels_nb = 1;
+
+	double signal[data_nb];
+
+	CreerNote(frequence_note,sampling_freq, data_nb, signal);
+	//std::cout << signal[400] << std::endl;
+
+	unsigned char data8[data_nb];
+	for(int i = 0; i < data_nb; i++){
+		data8[i] = dtoc(signal[i]);
+	}
+	
+	// le temps du son = data_nb/sampling_freq
+	Wave w(data8, data_nb, channels_nb, sampling_freq);
+
+	w.write("test.wav");
+
+	return 0;
+}
+/**/
